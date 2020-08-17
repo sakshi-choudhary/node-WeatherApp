8a20997b715a04348bdc625f1d1a3f19c1ee4bca
Sakshi Choudhary <choudharysakshi023@gmail.com>
2020-08-16 20:37:42 -0700

    styled weather result text

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,30 +1,29 @@
+const userinput = document.querySelector("form");
+const search = document.querySelector("input");
+const temp = document.querySelector("h2");
+const msgone = document.querySelector("#msg1");
 
+userinput.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+  temp.textContent = "Loading...";
+  msgone.textContent = "";
 
-
-const userinput= document.querySelector('form')
-const search=document.querySelector('input')
-const msgone=document.querySelector('#msg1')
-const msgtwo=document.querySelector('#msg2')
-
-userinput.addEventListener('submit',(e)=>{
-  e.preventDefault()
-const location=search.value
-msgone.textContent= ''
-msgtwo.textContent='Loading...'
-
-fetch(`/weather?address=${location}`).then((response) => {
+  fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-            msgone.textContent=' '
-           msgtwo.textContent=data.error
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-           msgone.textContent= data.location
-           msgtwo.textContent=data.forecast
-        }
-    })
-})
+      if (data.error) {
+        console.log(data.error);
+        temp.textContent = " ";
 
-})
+        msgone.textContent = data.error;
+      } else {
+        console.log(data.location);
+        console.log(data.forecast);
+
+        msgone.textContent = data.location;
+
+        temp.textContent = data.forecast;
+      }
+    });
+  });
+});
